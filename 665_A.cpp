5948252d0e215775683eb35d66df295f2ea86e9d
Suchet Aggarwal <44972944+Suchet-Agg@users.noreply.github.com>
2022-10-02 17:26:06 +0530

    added solution to 665-A

---
+++ 665_A.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+ 
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fastIO() ios_base::sync_with_stdio(false);  cin.tie(NULL);  cout.tie(NULL); 
+#define endl '\n'
+#define all(a) a.begin(),a.end()
+
+#define TRACE
+#ifndef ONLINE_JUDGE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+    cerr << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+    const char* comma = strchr(names + 1, ',');cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+}
+#else
+#define trace(...)
+#endif
+
+template <typename T>
+using ordered_set =tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T> ostream& operator << (ostream &os, const vector<T> &v) { for (T i : v) os << i << ' '; return os; }
+template <class T> ostream& operator << (ostream &os, const set<T> &v) { for (T i : v) os << i << ' '; return os; }
+template <class T, class S> ostream& operator << (ostream &os, const pair<T, S> &v) { os << v.first << ' ' << v.second; return os; }
+template <class T, class S> ostream& operator << (ostream &os, const unordered_map<T, S> &v) { for (auto i : v) os << '(' << i.first << " => " << i.second << ')' << ' '; return os; }
+template <class T, class S> ostream& operator << (ostream &os, const map<T, S> &v) { for (auto i : v) os << '(' << i.first << " => " << i.second << ')' << ' '; return os; }
+
+const int MAXN = 2*1e6 + 10;
+const int mod = 1e9 + 7;
+int t;
+
+// Testcase
+void testcase(){
+	int n,k;
+	cin>>n>>k;
+	if(n==k){
+		cout<<0<<endl;
+		return;
+	}
+	else{
+		if(k>n){
+			cout<<k-n<<endl;
+			return;
+		}
+		else{
+			if((n+k)%2!=0) cout<<1<<endl;
+			else cout<<0<<endl;
+		}
+	}
+}
+							
+//  Main
+signed main(){
+    fastIO();
+    // t = 1;
+    cin>>t;
+    while(t--){
+        testcase();
+    }
+    return 0;
+}
