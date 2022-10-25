7470b9aa1c9b9d6e87b0e2f54dfd616a82f0e962
Udhvarg Chaturvedi <42069852+udhvarg@users.noreply.github.com>
2022-10-25 12:15:08 +0300

    Create Watermelon-A.cpp

---
+++ Watermelon-A.cpp
@@ -0,0 +1,16 @@
+//Solution to problem https://codeforces.com/gym/226241/problem/A
+#include<iostream>
+using namespace std;
+int main()
+{
+	int w;
+	cin>>w;
+	if(w!=2 && w%2==0)
+	{
+		cout<<"YES";
+	}
+	else
+	{
+		cout<<"NO";
+	}
+}
